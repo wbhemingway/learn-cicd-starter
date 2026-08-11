cfaee68d0dd6a6bfe20b63de8c6f3a981c0bcd75
wbhemingway <william.b.hemingway@gmail.com>
2026-08-11 10:18:10 -0400

    cicd no longer breaks on tests

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -6,8 +6,7 @@ import (
 	"strings"
 )
 
-// var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
-var ErrNoAuthHeaderIncluded = errors.New("bozo")
+var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
